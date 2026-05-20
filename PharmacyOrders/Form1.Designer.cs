80136e233117cee4f297894b14ef1c4e569c0798
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-20 17:37:50 -0500

    adds initial controls

--- PharmacyOrders/Form1.Designer.cs
+++ PharmacyOrders/Form1.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.lblMedicamento = new System.Windows.Forms.Label();
             this.txtMedicamento = new System.Windows.Forms.TextBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblMedicamento
             // 
             this.lblMedicamento.AutoSize = true;
-            this.lblMedicamento.Location = new System.Drawing.Point(0, 0);
+            this.lblMedicamento.Location = new System.Drawing.Point(9, 33);
             this.lblMedicamento.Name = "lblMedicamento";
             this.lblMedicamento.Size = new System.Drawing.Size(113, 16);
             this.lblMedicamento.TabIndex = 0;
@@ -43,16 +46,46 @@
             // 
             // txtMedicamento
             // 
-            this.txtMedicamento.Location = new System.Drawing.Point(3, 19);
+            this.txtMedicamento.Location = new System.Drawing.Point(12, 52);
             this.txtMedicamento.Name = "txtMedicamento";
             this.txtMedicamento.Size = new System.Drawing.Size(100, 22);
             this.txtMedicamento.TabIndex = 1;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(139, 33);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(38, 20);
+            this.lblTipo.TabIndex = 2;
+            this.lblTipo.Text = "Type:";
+            this.lblTipo.UseCompatibleTextRendering = true;
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(139, 50);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipo.TabIndex = 3;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(298, 33);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(58, 16);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Quantity:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.lblTipo);
             this.Controls.Add(this.txtMedicamento);
             this.Controls.Add(this.lblMedicamento);
             this.Name = "Form1";
@@ -67,6 +100,9 @@
 
         private System.Windows.Forms.Label lblMedicamento;
         private System.Windows.Forms.TextBox txtMedicamento;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Label lblCantidad;
     }
 }
 
