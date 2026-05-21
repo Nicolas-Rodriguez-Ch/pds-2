893108f2a590c6949324c1517da45b5f681b250c
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-20 21:00:14 -0500

    creates new form view and adds labels to it

--- PharmacyOrders/Form1.cs
+++ PharmacyOrders/Form1.cs
@@ -38,14 +38,54 @@ namespace PharmacyOrders
         {
             if (string.IsNullOrWhiteSpace(txtMedicamento.Text))
             {
-                MessageBox.Show("Medication name cannot be empty");
+                MessageBox.Show("Medication name cannot be empty.");
                 return;
             }
-            if (string.IsNullOrWhiteSpace(txtCantidad.Text) || int.TryParse(txtCantidad.Text, out  int))
+            if (string.IsNullOrWhiteSpace(txtCantidad.Text))
             {
-                
+                MessageBox.Show("Medication quantity can't be empty.");
+                return;
             }
+            if (!int.TryParse(txtCantidad.Text, out int numero) || numero <= 0)
+            {
+                MessageBox.Show("Medication must be a number grater than 0");
+                return;
+            }
+            if (cmbTipo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Pleaase select a medication type");
+                return;
+            }
+            if (!rbCofarma.Checked && !rbEmpsephar.Checked && !rbCemefar.Checked)
+            {
+                MessageBox.Show("Please select a distributor");
+                return;
+            }
+            if (!chkPrincipal.Checked && !chkSecundaria.Checked)
+            {
+                MessageBox.Show("Please select at least one branch");
+                return;
+            }
+            string distribuidor = "";
 
+            if (rbCofarma.Checked)
+                distribuidor = "Cofarma";
+            else if (rbEmpsephar.Checked)
+                distribuidor = "Empsephar";
+            else if (rbCemefar.Checked)
+                distribuidor = "Cemefar";
+
+            string direccion = "";
+            if (chkPrincipal.Checked && chkSecundaria.Checked)
+                direccion = "Calle de la Rosa n. 28 and the one located at Calle Alcazabilla n. 3";
+            else if (chkPrincipal.Checked)
+                direccion = "Calle de la Rosa n. 28";
+            else if (chkSecundaria.Checked)
+                direccion = "Calle Alcazabilla n. 3";
+
+            string medicamento = txtMedicamento.Text.Trim();
+            string tipo = cmbTipo.SelectedItem.ToString();
+            int cantidad = int.Parse(txtCantidad.Text);
         }
     }
 }
---
+++ PharmacyOrders/Form2.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PharmacyOrders
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblMedicamentoLabel = new System.Windows.Forms.Label();
+            this.lblMedicamentoValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(63, 72);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(101, 16);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Order Summary";
+            // 
+            // lblMedicamentoLabel
+            // 
+            this.lblMedicamentoLabel.AutoSize = true;
+            this.lblMedicamentoLabel.Location = new System.Drawing.Point(346, 72);
+            this.lblMedicamentoLabel.Name = "lblMedicamentoLabel";
+            this.lblMedicamentoLabel.Size = new System.Drawing.Size(73, 16);
+            this.lblMedicamentoLabel.TabIndex = 1;
+            this.lblMedicamentoLabel.Text = "Medication";
+            // 
+            // lblMedicamentoValue
+            // 
+            this.lblMedicamentoValue.AutoSize = true;
+            this.lblMedicamentoValue.Location = new System.Drawing.Point(590, 71);
+            this.lblMedicamentoValue.Name = "lblMedicamentoValue";
+            this.lblMedicamentoValue.Size = new System.Drawing.Size(89, 16);
+            this.lblMedicamentoValue.TabIndex = 2;
+            this.lblMedicamentoValue.Text = "{placeholder}";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMedicamentoValue);
+            this.Controls.Add(this.lblMedicamentoLabel);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblMedicamentoLabel;
+        private System.Windows.Forms.Label lblMedicamentoValue;
+    }
+}
